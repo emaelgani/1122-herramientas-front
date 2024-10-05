5510cd0fe7ff6044b7b98e4c40d56481124bb3ab
emaelgani <emanuelelgani@gmail.com>
2024-10-05 17:46:32 -0300

    agrego manejo de errores al get cliente

--- src/app/cliente/services/cliente.service.ts
+++ src/app/cliente/services/cliente.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { environments } from 'src/environments/environments';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { Cliente, ClienteCantidadProducto } from '../../shared/interfaces/cliente.interface';
 import { Pedido } from 'src/app/shared/interfaces/pedido.interface';
 
@@ -17,36 +17,59 @@ export class ClienteService {
   constructor() { }
 
 
-  getClientes(): Observable<Cliente[]>{
-    return this.http.get<Cliente[]>(`${this.url}/api/Cliente`);
+  getClientes(): Observable<Cliente[]> {
+    return this.http.get<Cliente[]>(`${this.url}/api/Cliente`)
+      .pipe(
+        // Capturamos el error
+        catchError(this.handleError)
+      );
   }
 
-  getCuatroClientesConMasDeuda(): Observable<Cliente[]>{
+  getCuatroClientesConMasDeuda(): Observable<Cliente[]> {
     return this.http.get<Cliente[]>(`${this.url}/api/Cliente/CuatroConMasDeuda`);
   }
 
-  updateCliente(cliente: Cliente): Observable<Cliente[]>{
-    if(!cliente.idCliente) throw Error('El id del cliente es requerido');
+  updateCliente(cliente: Cliente): Observable<Cliente[]> {
+    if (!cliente.idCliente) throw Error('El id del cliente es requerido');
     return this.http.put<Cliente[]>(`${this.url}/api/Cliente`, cliente);
   }
 
-  createCliente(cliente: Cliente): Observable<Cliente[]>{
+  createCliente(cliente: Cliente): Observable<Cliente[]> {
     return this.http.post<Cliente[]>(`${this.url}/api/Cliente`, cliente);
   }
 
-  deleteCliente(id: number): Observable<Cliente[]>{
+  deleteCliente(id: number): Observable<Cliente[]> {
     return this.http.delete<Cliente[]>(`${this.url}/api/Cliente/${id}`);
   }
 
-  createNota(pedido: Pedido){
+  createNota(pedido: Pedido) {
     return this.http.post<Pedido[]>(`${this.url}/api/Pedido`, pedido);
   }
 
-  getTotalDeuda(): Observable<number>{
+  getTotalDeuda(): Observable<number> {
     return this.http.get<number>(`${this.url}/api/Cliente/TotalDeuda`);
   }
 
-  getClientesVentaProducto(idProducto: number): Observable<ClienteCantidadProducto[]>{
+  getClientesVentaProducto(idProducto: number): Observable<ClienteCantidadProducto[]> {
     return this.http.get<ClienteCantidadProducto[]>(`${this.url}/api/Cliente/ClientesCompraronProducto/${idProducto}`);
+  }
+
+  // Manejador de errores
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+
+    if (error.error instanceof ErrorEvent) {
+      // Error en el lado del cliente
+      errorMessage = `Client-side error: ${error.error.message}`;
+    } else {
+      // Error en el lado del servidor
+      errorMessage = `Server-side error: ${error.status} - ${error.message}`;
+    }
+
+    // Aquí podrías mostrar una alerta o notificación de error
+    console.error(errorMessage);
+
+    // Retornar un observable con el error para que pueda ser gestionado por el componente que consume el servicio
+    return throwError(() => new Error(errorMessage));
   }
 }
--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,5 +1,4 @@
 export const environments = {
   baseUrl: 'http://localhost:80'
-  //baseUrl: 'https://localhost'
-  //baseUrl: 'https://localhost:7073'
+
 }
